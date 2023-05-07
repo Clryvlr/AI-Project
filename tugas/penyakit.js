2e6d8a0a7c7cfd7bd0d54ad248383285ddd7d092
Clryvlr <fionnacaroline@mhs.mdp.ac.id>
2023-05-07 16:44:22 +0700

    Project 1.0

--- tugas/penyakit.js
+++ tugas/penyakit.js
@@ -12,57 +12,61 @@ form.addEventListener('submit', function(event) {
   prediksiPenyakit(gejala);
 });
 
+  
+  function prediksiPenyakit(gejalaInput) {
+
+    const gejalaInput = Array.from(document.querySelectorAll('input[name="demam"]:checked'))
+    .concat(Array.from(document.querySelectorAll('input[name="nyeri"]:checked')))
+    .map(el => el.value);
 
 
-const Penyakit = {
-    "Contract Ulcers": ["3", "16"],
-    "ABAES PARAFARINGEAL": ["3", "19"],
-    "ABAES PERITONAILER": ["1", "2", "7", "14", "16", "22"],
-    "BAROTITIS MEDIA": ["2", "6"],
-    "DEVIASI SEPTUM": ["1", "5", "6", "15", "25", "29"],
-    "FARINGITIS": ["1", "3", "7", "13", "14"],
-    "KANKER LARING": ["3", "4", "7", "13", "16", "23", "24"],
-    "KANKER LEHER DAN KEPALA": ["3", "12", "15", "21", "30", "31"],
-    "KANKER LEHER METASTATIK": ["12"],
-    "KANKER NASOFARING": ["5", "15"],
-    "KANKER TONSIL": ["7", "12"],
-    "LARINGITIS": ["1", "3", "14", "19", "37"],
-    "NEURONITIS VESTIBULARIS":	["10", "17"],
-    "OSTEOSKLEROSIS":	["20", "35"],
-    "OTITIS MEDIA AKUT":	["1", "6","10", "32"],
-    "MENIERE":	["6", "10", "34", "36"],
-    "TONSILITIS":	["1", "2", "3", "4", "7", "10"],
-    "TUMOR SYARAF PENDENGARAN": ["2", "20", "38"],
-    "VERTIGO POSTULAR":	["17"],
-    "SINUSITIS MAKSILARIS":	["1", "2", "4", "5", "8", "9", "11", "28", "33"],
-    "SINUSITIS FRONTALIS":	["1", "2", "4", "5", "8", "9", "11", "18"],
-    "SINUSITIS ETMOIDALIS":	["1", "2", "4", "5", "8", "9", "11", "18", "26", "27"],
-    "SINUSITIS SFENOIDALIS":	["1", "2", "4", "5", "6", "8", "9", "11", "12"],
-    "PERUT": ["1", "2", "3", "4"]
-  };
+    const gejalaPenyakit = {
+      "Contract Ulcers": ["3", "16"],
+      "ABAES PARAFARINGEAL": ["3", "19"],
+      "ABAES PERITONAILER": ["1", "2", "7", "14", "16", "22"],
+      "BAROTITIS MEDIA": ["2", "6"],
+      "DEVIASI SEPTUM": ["1", "5", "6", "15", "25", "29"],
+      "FARINGITIS": ["1", "3", "7", "13", "14"],
+      "KANKER LARING": ["3", "4", "7", "13", "16", "23", "24"],
+      "KANKER LEHER DAN KEPALA": ["3", "12", "15", "21", "30", "31"],
+      "KANKER LEHER METASTATIK": ["12"],
+      "KANKER NASOFARING": ["5", "15"],
+      "KANKER TONSIL": ["7", "12"],
+      "LARINGITIS": ["1", "3", "14", "19", "37"],
+      "NEURONITIS VESTIBULARIS":	["10", "17"],
+      "OSTEOSKLEROSIS":	["20", "35"],
+      "OTITIS MEDIA AKUT":	["1", "6","10", "32"],
+      "MENIERE":	["6", "10", "34", "36"],
+      "TONSILITIS":	["1", "2", "3", "4", "7", "10"],
+      "TUMOR SYARAF PENDENGARAN": ["2", "20", "38"],
+      "VERTIGO POSTULAR":	["17"],
+      "SINUSITIS MAKSILARIS":	["1", "2", "4", "5", "8", "9", "11", "28", "33"],
+      "SINUSITIS FRONTALIS":	["1", "2", "4", "5", "8", "9", "11", "18"],
+      "SINUSITIS ETMOIDALIS":	["1", "2", "4", "5", "8", "9", "11", "18", "26", "27"],
+      "SINUSITIS SFENOIDALIS":	["1", "2", "4", "5", "6", "8", "9", "11", "12"],
+      "PERUT": ["1", "2", "3", "4"]
+    };;
   
-  function prediksiPenyakit(gejala) {
-    const hasilPrediksi = [];
-  
-    for (const [namaPenyakit, daftarGejala] of Object.entries(Penyakit)) {
-      let jumlahMirip = 0;
-  
-      for (const gejalaPenyakit of daftarGejala) {
-        if (gejala.includes(gejalaPenyakit)) {
-          jumlahMirip++;
-        }
+    const daftarPenyakit = Object.keys(gejalaPenyakit);
+
+    let penyakitCocok = [];
+  daftarPenyakit.forEach(function(penyakit) {
+    let gejalaPenyakit = gejalaPenyakit[penyakit];
+    let cocok = true;
+    gejala.forEach(function(gejala) {
+      if (!gejalaPenyakit.includes(gejala)) {
+        cocok = false;
       }
-  
-      if (jumlahMirip > 0 && jumlahMirip >= Math.ceil(daftarGejala.length / 2)) {
-        hasilPrediksi.push(namaPenyakit);
-      }
+    });
+    if (cocok) {
+      penyakitCocok.push(penyakit);
     }
-  
-    if (hasilPrediksi.length > 0) {
-      alert(`Berdasarkan gejala yang diinput, kemungkinan Anda menderita ${hasilPrediksi.join(', ')}`);
-    } else {
-      alert('Gejala yang diinput tidak cocok dengan penyakit apa pun yang terdapat pada database');
-    }
+  });
+  if (penyakitCocok.length === 0) {
+    return "Tidak ditemukan penyakit yang cocok dengan gejala-gejala yang diinput.";
+  } else {
+    return "Berdasarkan gejala-gejala yang diinput, kemungkinan Anda mengalami penyakit " + penyakitCocok.join(", ") + ".";
+  }
   }
 
   function clearCheckbox() {
